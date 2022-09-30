ba0f4e16436dac219e37d07fb141c5d44b7101c3
ThomasBaeskow <thomasbaeskow@web.de>
2022-09-30 11:53:36 +0200

    setting up eslint and prettier with individual configuration

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-import express from "express"
-import morgan from "morgan"
-import tourRouter from "./routes/tourRoutes.js"
-import userRouter from "./routes/userRoutes.js"
-import path from "path"
-import dotenv from "dotenv"
+/* eslint-disable import/extensions */
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import dotenv from 'dotenv';
+import tourRouter from './routes/tourRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
-dotenv.config({ path: "./config.env" })
+dotenv.config({ path: './config.env' });
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-const app = express()
+const app = express();
 
 // 1. MIDDLEWARE (express build in middleware) We just want to use morgan middleware when we are in development, not in production.
-if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev")) 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
-app.use(express.json())
-app.use(express.static(`${__dirname}/public`)) // when we type now in our browser 127.0.0.1:3000/overview.html we can see our html file dislayed in te browser. We can do that with all our static files 
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`)); // when we type now in our browser 127.0.0.1:3000/overview.html we can see our html file dislayed in te browser. We can do that with all our static files
 // Creating our own middleware function!:
-app.use((req,res,next) => {
-    console.log("Hello from the middleware!");
-    next()
-})
- // requesttime for every request added to the request object as a key.
-app.use((req,res,next) => {
-    req.requestTime = new Date().toISOString();
-    console.log(req.requestTime);
-    next()
-})
+app.use((req, res, next) => {
+  console.log('Hello from the middleware!');
+  next();
+});
+// requesttime for every request added to the request object as a key.
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log(req.requestTime);
+  next();
+});
 
 // 3. ROUTES:
-app.use("/api/v1/tours", tourRouter)
-app.use("/api/v1/users", userRouter)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-export default app
+export default app;
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-import app from "./app.js"
-import express from "express"
+/* eslint-disable import/extensions */
+import app from './app.js';
 
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
-})
+  console.log(`App running on port ${port}...`);
+});
 
 // USE "NPM START" in terminal to run the server. We changed the script in package.json from test to start with value "nodemon server.js"
