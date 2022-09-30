3d33f9cdf7f0d949ecc37ea993f308368df7349f
ThomasBaeskow <thomasbaeskow@web.de>
2022-09-30 11:55:14 +0200

    setting up eslint and prettier with individual configuration

--- server.js
+++ server.js
@@ -8,3 +8,30 @@ app.listen(port, () => {
 });
 
 // USE "NPM START" in terminal to run the server. We changed the script in package.json from test to start with value "nodemon server.js"
+
+// 1. install following dependencies in next project like:
+// npm i eslint prettier eslint-config-prettier eslint-plugin-prettier eslint-config-airbnb eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react --save-dev
+// 2. create .eslintrc.json and copy inside:
+// {
+//   "extends": ["airbnb", "prettier", "plugin:node/recommended"],
+//   "plugins": ["prettier"],
+//   "rules": {
+//     "prettier/prettier": "error",
+//     "spaced-comment": "off",
+//     "no-console": "off",
+//     "consistent-return": "off",
+//     "func-names": "off",
+//     "object-shorthand": "off",
+//     "no-process-exit": "off",
+//     "no-param-reassign": "off",
+//     "no-return-await": "off",
+//     "no-underscore-dangle": "off",
+//     "class-methods-use-this": "off",
+//     "prefer-destructuring": ["error", { "object": true, "array": false }],
+//     "no-unused-vars": ["error", { "argsIgnorePattern": "req|res|next|val" }]
+//   }
+// }
+// 3. create .prettierrc and copy following inside:
+// {
+//   "singleQuote": true
+// }
