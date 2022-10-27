78b2a69e64eadb3790d255d61fc1ace50993c430
ThomasBaeskow <thomasbaeskow@web.de>
2022-10-27 12:54:25 +0200

    adding express-mongo-sanitize, helmet, express-rate-limit, xss packages for setting security HTTP Headers, Rate Limiting, Data Sanitization

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ import userRouter from './routes/userRoutes.js';
 import AppError from "./utils/appError.js"
 import {globalErrorHandler} from "./controllers/errorController.js"
 import rateLimit from "express-rate-limit"
+import helmet from "helmet"
+import mongoSanitize from "express-mongo-sanitize"
+import xss from "xss-clean"
 
 dotenv.config({ path: './config.env' });
 
@@ -15,11 +18,18 @@ const __dirname = path.resolve();
 
 const app = express();
 
-// GLOBAL MIDDLEWARE 
+// GLOBAL MIDDLEWARES
+
+// 1) Set security HTTP headers
+// install helmet and call it. Its better to call the helmet early in your middleware stack to be sure, that the headers are set. (Postman you can see Headers)
+app.use(helmet())
+
+// 2) Development logging
 if (process.env.NODE_ENV === 'development') { // - We just want to use morgan middleware when we are in development, not in production.
   app.use(morgan('dev'));
 }
 
+// 3) Limit requests from same API
 // install express-rate-limit package for limiting requests from an IP using the function rateLimit()
 const limiter = rateLimit({
   max: 100, // count of requests limited
@@ -28,10 +38,26 @@ const limiter = rateLimit({
 })
 app.use("/api", limiter) // we just want to apply our limiter middleware for routes which start with "/api". When we restart our application (save), the limit resets
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }))
+// 4) Body parser, reading data from body into req.body
+app.use(express.json({limit: "10kb"})); // we can add options to our .json middleware to limit the data which the client can send to our application. We limit to10 kilobyte
+
+// 5) Data sanitization 
+// a) against NoSQL query injection
+app.use(mongoSanitize()) // it looks in req.body.query and filter out all "$" and "."
+// when we pass as user input a query, we can log in to a users Account without knowing the email address. Program creates a valid token etc... Thats why we need to install "express-mongo-sanitize"
+// {
+//   "email": {"$gt": ""},
+//   "password": "ja%nine1990"
+// }
+
+// b) against XSS - HTML and js script input
+app.use(xss())
+
+// 6) Serving static files
 app.use(express.static(`${__dirname}/public`)); // when we type now in our browser 127.0.0.1:3000/overview.html we can see our html file displayed in te browser. We can do that with all our static files
 
+// 7) Test middleware
 // request time for every request added to the request object as a key.
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -39,7 +65,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// 3. ROUTES:
+// ROUTES:
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
@@ -54,16 +80,6 @@ app.all("*", (req, res, next) => {
 // Jonas global error handler:
 // by implementing 4 arguments (parameters) express knows, that this is a global error handling middleware
 app.use(globalErrorHandler)
-
-// Dilshod global error handler:
-// app.use((error, req, res, next) => {
-//   res.status(error.status || 500);
-//   res.json({
-//     error: {
-//       message: error.message,
-//     },
-//   });
-// });
 
 
 export default app;
